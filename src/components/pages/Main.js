e4f0b6af11374f4ba8e012661b3e9f9068d08c5b
Rafal Cybula <242370@edu.p.lodz.pl>
2024-11-24 13:44:39 +0100

    Version 1.0

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -10,8 +10,13 @@ const Main = () =>
     const zgierzText = "Zgierz";
     const radomskoText = "Radomsko";
     const entryText = 'Witamy na stronie basenów GżdyPuciek – dowiedz się więcej o największych i najlepszych atrakcjach wodnych w kraju. Zebrane w tym miejscu informacje, pozwolą Ci zaplanować dzień, a nawet cały weekend nad wodą. Wybieraj spośród naszych lokalizacji, spędź czas nad basenem. Rozpocznij dzień nad wodą!';
-
     const bargainText = 'Tylko w najbliższą sobotę wszystkie dzieci do 10% taniej!';
+    const lodzTitle = 'Łódź - Bajoro';
+    const zgierzTitle = 'Zgierz - Kałuża';
+    const radomskoTitle = 'Radomsko - Złote Jezioro';
+    const lodzDescription = 'Najnowocześniejszy i jeden z największych basenów w Polsce, zlokalizowany w samym centrum miasta. Połączenie klasycznego basenu z centrum edukacyjnym i ogromną strefą SPA&Wellness . 17 wewnętrznych basenów, największe saunarium w Polsce i 11 zjeżdżalni o łącznej długości pół kilometra!';
+    const zgierzDescription = 'Jeden z największych basenów woj. łódzkiego zlokalizowany 20 minut drogi od Łodzi w miejscowości Zgierz. Oferuje m.in zjeżdżalnie o długości 21 i 37 metrów, basen sportowy oraz basen ze sztuczną falą!';
+    const radomskoDescription = 'Najnowocześniejszy i jeden z największych basenów w Polsce, zlokalizowany w samym centrum miasta. Połączenie klasycznego basenu z centrum edukacyjnym i ogromną strefą SPA&Wellness . 17 wewnętrznych basenów, największe saunarium w Polsce i 11 zjeżdżalni o łącznej długości pół kilometra!';
 
     return(
       <div className="Main">
@@ -34,6 +39,29 @@ const Main = () =>
               </div>
           </div>
           <p className="EntryText">{entryText}</p>
+          <div className="Descriptions">
+              <div className="DescriptionContainer">
+                  <div className="DescriptionImage1"/>
+                  <div className="tmp">
+                      <p className="DescriptionTitle">{lodzTitle}</p>
+                      <p className="Description">{lodzDescription}</p>
+                  </div>
+              </div>
+              <div className="DescriptionContainer1">
+                  <div className="tmp">
+                      <p className="DescriptionTitle1">{zgierzTitle}</p>
+                      <p className="Description1">{zgierzDescription}</p>
+                  </div>
+                  <div className="DescriptionImage2"/>
+              </div>
+              <div className="DescriptionContainer">
+                  <div className="DescriptionImage3"/>
+                  <div className="tmp">
+                      <p className="DescriptionTitle">{radomskoTitle}</p>
+                      <p className="Description">{radomskoDescription}</p>
+                  </div>
+              </div>
+          </div>
           <Footer/>
       </div>
     );
